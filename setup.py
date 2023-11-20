4553cf8a5f429d675429b027aef0a949e29b38b7
gunasimhareddy72 <gunasimhareddy72@gmail.com>
2023-11-20 20:14:19 +0000

    added setup.py to do application setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Guna',
+    author_email='10628714@mydbs.ie',
+    description='HospitalManagementSystem for DBS python course'
+)
